4f91773c2d70b5bc92f6223a9abdf7f119d62496
MarioDiCaprio <mariodicaprio10@gmail.com>
2023-03-17 13:09:43 +0100

    Parallax now dynamically calculates children and wrapping

--- src/components/ParallaxText/ParallaxText.tsx
+++ src/components/ParallaxText/ParallaxText.tsx
@@ -1,11 +1,11 @@
-import React, {ReactNode, useRef} from "react";
+import React, {ReactNode, useEffect, useRef, useState} from "react";
 import {useAnimationFrame, useMotionValue, useScroll, useSpring, useTransform, useVelocity} from "framer-motion";
-import {wrap} from "@motionone/utils";
 import {Child, Context, Scroller} from "./ParallaxText.styles";
+import {wrap} from "@motionone/utils";
 
 
 // DISCLAIMER
-// taken from:
+// idea taken from:
 // https://codesandbox.io/s/framer-motion-scroll-velocity-r1dy4u?from-embed=&file=/src/App.tsx:1946-2069
 
 
@@ -15,6 +15,19 @@ interface ParallaxTextProps {
 }
 
 const ParallaxText: React.FC<ParallaxTextProps> = ({ baseVelocity, children }) => {
+    // procedure to dynamically calculate number of children for the text
+    // to repeat itself in a minimal manner
+    const contextRef = useRef<HTMLDivElement>(null);
+    const childRef = useRef<HTMLSpanElement>(null);
+    const [numberOfChildren, setNumberOfChildren] = useState<number>(0);
+    useEffect(() => {
+        let contextWidth = contextRef.current?.clientWidth;
+        let childWidth = childRef.current?.clientWidth;
+        if (contextWidth && childWidth) {
+            setNumberOfChildren(Math.ceil(contextWidth / childWidth));
+        }
+    }, [contextRef, childRef]);
+
     const baseX = useMotionValue(0);
     const { scrollY } = useScroll();
     const scrollVelocity = useVelocity(scrollY);
@@ -26,12 +39,9 @@ const ParallaxText: React.FC<ParallaxTextProps> = ({ baseVelocity, children }) =
         clamp: false
     });
 
-    /**
-     * This is a magic wrapping for the length of the text - you
-     * have to replace for wrapping that works for you or dynamically
-     * calculate
-     */
-    const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
+    // wraps text between (-100% / n) and 0%, where n is the number of total children. For example,
+    // if we have 4 children, then the text is wrapped between -25% and 0%
+    const x = useTransform(baseX, (v) => `${wrap(-50/numberOfChildren, 0, v)}%`);
 
     const directionFactor = useRef<number>(1);
     useAnimationFrame((_t, delta) => {
@@ -50,11 +60,31 @@ const ParallaxText: React.FC<ParallaxTextProps> = ({ baseVelocity, children }) =
     });
 
     return (
-        <Context>
+        <Context ref={contextRef}>
+
+            {/* This component moves on the x-axis to create the animation */}
             <Scroller style={{ x }}>
-                <Child>{children} </Child>
-                <Child>{children} </Child>
-                <Child>{children} </Child>
+
+                {/* child width is calculated from this component */}
+                <Child key={0} ref={childRef}>
+                    {children}
+                </Child>
+
+                {/* create more children based on calculated number */}
+                {
+                    (function () {
+                        let childList: ReactNode[] = [];
+                        for (let i=0; i<numberOfChildren; i++) {
+                            childList.push(
+                                <Child key={i + 1}>
+                                    { children }
+                                </Child>
+                            );
+                        }
+                        return childList;
+                    }())
+                }
+
             </Scroller>
         </Context>
     );
